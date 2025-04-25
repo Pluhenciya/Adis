e046ba18f6d6a68cfe12f6ddfcae5457ab5ce1c2
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2025-04-25 17:19:50 +0300

    add model user

---
+++ Adis.Dm/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adis.Dm
+{
+    public enum Role
+    {
+        Admin,
+        Projecter
+    }
+}
---
+++ Adis.Dm/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adis.Dm
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string PasswordHash { get; set; } = null!;
+
+        public Role Role { get; set; }
+
+        public string? FullName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
