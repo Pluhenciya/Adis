01d89bb185dcf7bd2ac21324c0c57d96e3e5bfe6
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2025-05-19 01:54:38 +0300

    fix docker

--- adis.client/docker-entrypoint.sh
+++ adis.client/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "Processing environment variables..."
 CONFIG='{"yandexMapsApiKey": "'"${YANDEX_MAPS_API_KEY}"'"}'
 
-# Ищем JavaScript файлы для замены конфигурации
-find /usr/share/nginx/html -name '*.js' -exec sed -i "s|APP_CONFIG = {};|APP_CONFIG = ${CONFIG};|g" {} +
+# Исправленная команда find
+find /usr/share/nginx/html -name '*.js' -exec sed -i "s|APP_CONFIG = {};|APP_CONFIG = ${CONFIG};|g" {} \;
 
 exec "$@"
